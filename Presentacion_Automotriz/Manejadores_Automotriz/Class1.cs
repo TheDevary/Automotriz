48a1b8dd3021facaaec43f6300ed91dec4777284
Ariel <ari.reyes195@gmail.com>
2022-10-06 08:51:38 -0500

    Creacion de las 4 Capas

---
+++ Presentacion_Automotriz/Manejadores_Automotriz/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejadores_Automotriz
+{
+    public class Class1
+    {
+    }
+}
